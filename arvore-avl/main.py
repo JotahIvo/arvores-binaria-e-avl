51a780fbd5c6007a3d9d1d351f9196431cddb57d
João Pedro de Almeida Ivo <105402804+JotahIvo@users.noreply.github.com>
2023-11-17 20:09:10 -0300

    Update main.py

--- arvore-avl/main.py
+++ arvore-avl/main.py
@@ -1,40 +1,21 @@
-from noh_arvore_avl import NohArvoreAVL
+from noh_arvore_avl import No_AVL
 
 
 def imprimir_arvore(arvore, nivel=0, prefixo='Raiz: '):
     if arvore is not None:
-        print(' ' * (nivel * 4) + prefixo + str(arvore.info))
+        print(' ' * (nivel * 4) + prefixo + str(arvore.data) + " - FB: " + str(arvore.balanceamento))
         if arvore.esquerda is not None or arvore.direita is not None:
             imprimir_arvore(arvore.esquerda, nivel + 1, 'ESQ: ')
             imprimir_arvore(arvore.direita, nivel + 1, 'DIR: ')
 
 
-def retorna_altura(noh):
-    if noh is None:
-        return 0
-    return noh.altura
+arvore_avl = No_AVL(1)
 
+arvore_avl.insere(2)
+arvore_avl.insere(3)
+arvore_avl.insere(4)
+arvore_avl.insere(5)
+arvore_avl.insere(6)
+arvore_avl.insere(7)
 
-def balanco(noh):
-    if noh is None:
-        return 0
-    return retorna_altura(noh.esquerda) - retorna_altura(noh.direita)
-
-
-#criando uma arvore avl já balanceada:
-raiz = NohArvoreAVL(4, 3)
-raiz.esquerda = NohArvoreAVL(2, 2)
-raiz.direita = NohArvoreAVL(6, 2)
-raiz.esquerda.esquerda = NohArvoreAVL(1, 1)
-raiz.esquerda.direita = NohArvoreAVL(3, 1)
-raiz.direita.esquerda = NohArvoreAVL(5, 1)
-raiz.direita.direita = NohArvoreAVL(7, 1)
-
-raiz_nao_balanceada = NohArvoreAVL(1, 3)
-raiz_nao_balanceada.direita = NohArvoreAVL(2, 2)
-raiz_nao_balanceada.direita.direita = NohArvoreAVL(3, 1)
-
-imprimir_arvore(raiz)
-print("O fator de balandeamento da raiz é: ", balanco(raiz), "\n")
-imprimir_arvore(raiz_nao_balanceada)
-print("O fator de balandeamento da raiz é: ", balanco(raiz_nao_balanceada), "\n")
+imprimir_arvore(arvore_avl)
