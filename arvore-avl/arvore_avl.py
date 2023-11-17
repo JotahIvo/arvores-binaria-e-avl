f3c32bb47d99221eb5d6f228cb0b794a9c878430
João Pedro de Almeida Ivo <105402804+JotahIvo@users.noreply.github.com>
2023-11-17 20:08:53 -0300

    Update arvore_avl.py

--- arvore-avl/arvore_avl.py
+++ arvore-avl/arvore_avl.py
@@ -1,10 +1,1 @@
-def altura_arvore(noh):
-        if noh is None:
-            return 0
-        return noh.altura
 
-
-def fator_balanceamento(noh):
-    if noh is None:
-        return 0
-    return altura_arvore(noh.esquerda) - altura_arvore(noh.direita)
